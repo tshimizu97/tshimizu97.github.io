14462059b8de4a7410133b35257a282a7b1f6ef2
tshimizu97 <tshimizu.midd@gmail.com>
2021-01-02 13:51:01 +0900

    Added links to social media

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -26,12 +27,21 @@ export default function Home() {
             </p>
 
             <p>
-              My academic interests include fairness and privacy in ML, ML applications in bioinformatics and computational neuroscience.
+              My academic interests include fairness and privacy in ML, ML applications in bioinformatics, and computational neuroscience.
             </p>
 
             <p>
               In my free time, I enjoy bouldering, eating out with friends and family, and cooking.
             </p>
+
+            <div className={styles.logos}>
+              <Link href="https://www.linkedin.com/in/takao-shimizu-880b2814a/">
+                <img className={styles.logo} src="linkedin_logo.png" alt="linkedin" />
+              </Link>
+              <Link href="https://github.com/tshimizu97">
+                <img className={styles.logo} src="github_logo.png" alt="github" />
+              </Link>
+            </div>
           </article>
         </div>
 
